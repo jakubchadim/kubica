f2086ce78457bf14a1335a66620f44ea4e07364b
Jakub Chadim <jakub.chadim@rtsmunity.com>
2019-06-02 12:12:37 +0200

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RouteHome from './routes/Home'
+
+const App: React.FC = () => (
+  <RouteHome />
+)
+
+export default App
---
+++ src/routes/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RouteHome: React.FC = () => (
+  <div>
+    <h1>Kubiča</h1>
+  </div>
+)
+
+export default RouteHome
